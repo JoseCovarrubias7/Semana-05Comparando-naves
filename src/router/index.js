2d3edf12a721a272b4b6e6736e4d374f6c28cc7a
JoseCovarrubias7 <99362465+JoseCovarrubias7@users.noreply.github.com>
2022-03-30 10:37:53 -0700

    Router

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/sabores',
+    name: 'sabores',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Sabores.vue')
+  },
+
+  {
+    path: '/adornos',
+    name: 'adornos',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Adornos.vue')
+  },
+
+  {
+    path: '/pastelero',
+    name: 'pastelero',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Pastelero.vue')
+  },
+]
+
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
