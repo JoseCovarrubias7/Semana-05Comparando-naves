94baed5e4e3e0911487df00a2b489d56ba89e56c
JoseCovarrubias7 <99362465+JoseCovarrubias7@users.noreply.github.com>
2022-03-31 12:13:38 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   publicPath: '/Semana-05Comparando-naves',
 }
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
