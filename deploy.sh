f98d73bcce908544a609821a59a1de22991439d4
JoseCovarrubias7 <99362465+JoseCovarrubias7@users.noreply.github.com>
2022-03-31 12:27:55 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# if you are deploying to https://JoseCovarrubias7.github.io
+# git push -f git@github.com:JoseCovarrubias7/JoseCovarrubias7.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# if you are deploying to https://JoseCovarrubias7.github.io/Semana-05Comparando-naves
+# git push -f git@github.com:JoseCovarrubias7/Semana-05Comparando-naves.git main
 
 cd -
